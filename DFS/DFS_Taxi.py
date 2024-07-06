6fb66d5164b939b238e98cb861ff52a66e41a94c
P4Tr1K25 <145812130+P4Tr1K25@users.noreply.github.com>
2024-07-06 17:08:43 -0400

    Add files via upload
    Cambio a la vista de humano

--- DFS/DFS_Taxi.py
+++ DFS/DFS_Taxi.py
@@ -4,49 +4,66 @@ import matplotlib.pyplot as plt
 from gymnasium.envs.toy_text.taxi import TaxiEnv
 
 def dfs(env, state, visited, path, passenger_picked, TE):
+    # Decode the current state
     taxi_row, taxi_col, passenger, dest = TE.unwrapped.decode(state)
-    
-    if passenger == dest:
-        return path, 1  # Return path and reward of 1 for reaching the goal
 
+    # Check if the state has already been visited
     if state in visited:
-        return None, 0  # If state already visited, return None
+        return None, 0
 
+    # Mark this state as visited
     visited.add(state)
 
-    # If passenger not picked up and we are at passenger's location
+    # If the passenger is picked up and we're at the destination, drop off the passenger
+    if passenger_picked and (taxi_row == env.locs[dest][0] and taxi_col == env.locs[dest][1]):
+        action = 5  # Dropoff action
+        env.unwrapped.s = state  # Restore state
+        next_state, _, done, _, _ = env.step(action)
+        if done:
+            return path + [action], 1
+
+    # If the passenger is not picked up and we're at the passenger's location, pick up the passenger
     if not passenger_picked and passenger < 4 and (taxi_row == env.locs[passenger][0] and taxi_col == env.locs[passenger][1]):
         action = 4  # Pickup action
+        env.unwrapped.s = state  # Restore state
         next_state, _, _, _, _ = env.step(action)
-        return dfs(env, next_state, visited, path + [action], True, TE)
+        result, reward = dfs(env, next_state, visited, path + [action], True, TE)
+        if reward > 0:
+            return result, reward
 
-    # If passenger picked up and we are at destination
-    if passenger_picked and passenger == 4 and (taxi_row == env.locs[dest][0] and taxi_col == env.locs[dest][1]):
-        action = 5  # Dropoff action
-        next_state, _, _, _, _ = env.step(action)
-        return path + [action], 1  # Return path and reward of 1 for reaching the goal
+    # Explore all possible actions (0: south, 1: north, 2: east, 3: west)
+    for action in range(4):
+        env.unwrapped.s = state  # Restore state
+        next_state, _, done, _, _ = env.step(action)
+        if done:
+            return path + [action], 1
+        result, reward = dfs(env, next_state, visited, path + [action], passenger_picked, TE)
+        if reward > 0:
+            return result, reward
 
-    possible_moves = [0, 1, 2, 3]  # South, North, East, West
-    for action in possible_moves:
-        env.unwrapped.s = state  # Restore the state
-        next_state, _, _, _, _ = env.step(action)
-        subpath, subreward = dfs(env, next_state, visited, path + [action], passenger_picked, TE)
-        if subpath is not None:
-            return subpath, subreward
-    
+    # Backtrack if no valid path found
     visited.remove(state)
-    return None, 0  # No valid path found, return 0 reward
+    return None, 0
 
 def run_dfs(episodes):
-    env = gym.make('Taxi-v3', render_mode=None)
+    env = gym.make('Taxi-v3')
+    TE = TaxiEnv()
 
     rewards_per_episode = np.zeros(episodes)
     
     for episode in range(episodes):
         initial_state = env.reset()[0]  # Obtain initial state
-        TE = TaxiEnv()
         visited = set()
         path, reward = dfs(env, initial_state, visited, [], False, TE)
+        
+        # # Muestra el camino encontrado para el modo humano
+        # if path:
+        #     env = gym.make('Taxi-v3', render_mode='human')
+        #     env.reset()
+        #     env.unwrapped.s = initial_state
+        #     for action in path:
+        #         env.step(action)
+        #         env.render()
         
         rewards_per_episode[episode] = reward
 
@@ -59,4 +76,4 @@ def run_dfs(episodes):
     plt.savefig('taxi_dfs.png')
 
 if __name__ == '__main__':
-    run_dfs(1000)
+    run_dfs(1)
